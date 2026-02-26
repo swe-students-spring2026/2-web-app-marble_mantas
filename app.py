951872dd642d964f763d7ac5baf406d50b2365e9
localhost433 <tc4119@nyu.edu>
2026-02-25 19:10:02 -0500

    Image paths at /static, and Flask config

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from routes.items import items_bp
 from routes.auth import auth_bp, User
 
 def create_app():
-    app = Flask(__name__, static_folder="images", static_url_path="/images")
+    app = Flask(__name__)
 
     # Configure secret key for session management
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "devsecretkeychangeme")
